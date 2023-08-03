17fc13e3efcbbfee91a748ceeb2bb172f51c291f
fitiha <105194158+fitiha@users.noreply.github.com>
2023-08-03 04:07:00 +0300

    Update MiniUpWork.cpp
    Deleted some of the unnecessary comments.

--- MiniUpWork.cpp
+++ MiniUpWork.cpp
@@ -1,23 +1,3 @@
-/*
-MANUAL FOR USING THE PLATFORM
-For Workers
-Use the "Find Work" section if you are a worker
- - Use "Register" to add your profile as a worker
- - Use "Edit Profile" to modify or delete your account profile
- - Use "Exit" to if you finish your task
-
-For Hirers
-Use the "Hire" section if you want peoples to hire
- - Use "Find by ID" to know the details of a worker using his/her ID
- - Use "Find by work type" to get the list of workers sorted by their job type
- - Use "Available Workers List" to find the details of registered available workers
- - Use "List Of Registered Work Types" to find the list of work types our workers got registered for
- - Use "Exit" to if you finish your task
-
- It's assumed that after an employer got the right type of worker he/she can contact
- the worker using the provided phone number and other informations
-
-*/
 #include<iostream>
 #include<conio.h>
 #include<fstream>
